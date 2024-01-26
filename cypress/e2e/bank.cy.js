123cdbe1b620103297a32a06757fb6e7dcb7440b
Luiz Antonio Nunes Neves <lznunes@gmail.com>
2024-01-25 21:24:49 -0300

    caso de teste login com falha

--- cypress/e2e/bank.cy.js
+++ cypress/e2e/bank.cy.js
@@ -5,4 +5,22 @@ describe('Acess the bank account', () => {
     cy.get('input[type="password"]').type('demo')
     cy.get('input[type="submit"]').should('be.visible').click()
   })
+
+  it('Log in  user and password empty', () => {
+    cy.visit('https://parabank.parasoft.com/')
+    cy.get('input[type="submit"]').should('be.visible').click()
+    cy.contains('Please enter a username and password.', {timeout:1000}).should('be.visible')
+  })
+
+  it('Log in with wrong user', () => {
+    cy.visit('https://parabank.parasoft.com/')
+    cy.get('input[type="text"]').type('jhon')
+    cy.get('input[type="password"]').type('demo1')
+    cy.get('input[type="submit"]').should('be.visible').click()
+    cy.contains('An internal error has occurred and has been logged.', {timeout:1000}).should('be.visible')
+  })
+
+
+
+
 })
